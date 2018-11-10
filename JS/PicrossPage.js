db98aafdd316ed82960e7ad25c8a06dbb3b037dc
Photoperiod <mphilipp622@gmail.com>
2018-11-09 16:24:00 -0800

    Updating Picross Page Arcade Mode

--- JS/PicrossPage.js
+++ JS/PicrossPage.js
@@ -32,17 +32,15 @@ function CreateGrid()
 function StartArcadeMode()
 {
     let numberOfLevels = document.getElementById("numberOfLevels").value;
-    let maxWidth = Math.floor(document.getElementById("maxWidth").value);
-    let maxHeight = Math.floor(document.getElementById("maxHeight").value);
-    let minWidth = Math.ceil(document.getElementById("minWidth").value);
-    let minHeight = Math.ceil(document.getElementById("minHeight").value);
+    let selection = document.getElementById("arcadeDimension");
+    let newDimension = parseInt(selection.options[selection.selectedIndex].value);
+    let newWidth = newDimension;
+    let newHeight = newDimension;
+    levels = [];
 
     // create as many levels as user specified. Max dimensions of 15x15. Min dimensions of 2x2
     for(let i = 0; i < numberOfLevels; i++)
     {
-        let newWidth = Math.floor(Math.random() * (maxWidth - minWidth)) + minWidth;
-        let newHeight = Math.floor(Math.random() * (maxHeight - minHeight)) + minHeight;
-
         tempGrid = new Grid(newWidth, newHeight);
 
         levels.push(tempGrid);
@@ -51,14 +49,10 @@ function StartArcadeMode()
     grid = levels[0];
     isArcadeMode = true;
 
-    console.log(grid);
-
     CreateTable();
     CreateTableOnClickFunctionality();
     UpdateGameStats();
-    RemoveGridOptions();
     StartTimer();
-    // document.location.href = "ArcadeMode.html";
 }
 
 function LoadNextLevel()
@@ -123,47 +117,6 @@ function UpdateGameStats()
     document.getElementById("TurnData").innerHTML = turns
     document.getElementById("TilesRemainingData").innerHTML = grid.GetRemainingTiles();
     document.getElementById("MistakeData").innerHTML = mistakes;
-}
-
-// WORK IN PROGRESS
-function CreateGridFromImage()
-{
-    let newWidth = document.getElementById("gridWidth").value;
-    let newHeight = document.getElementById("gridHeight").value;
-    grid = new Grid(newWidth, newHeight);
-
-    let img = document.getElementById("imageInput").value;
-    console.log(img);
-
-    let canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    console.log(canvas);
-
-    let context = canvas.getContext('2d');
-    context.drawImage(img, 0, 0, img.width, img.height);
-
-    // canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-
-    let imageData = context.getImageData(0,0,canvas.width,canvas.height);
-    let data = imageData.data;
-    let r,g,b,avg;
-    let colorSum = 0;
-
-    console.log(data.length);
-
-    // for(let x = 0; x < data.length; x += 4) {
-    //     // Each pixel stores 4 values. RGBA. Skip by 4 each iteration.
-    //     r = data[x];
-    //     g = data[x+1];
-    //     b = data[x+2];
-
-    //     avg = Math.floor((r+g+b)/3);
-    //     colorSum += avg;
-    // }
-
-    // let brightness = Math.floor(colorSum / (this.width*this.height));
-
 }
 
 function SaveGame()
